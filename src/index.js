9441e094bc4f94024bb28468705785673fc693b5
Kevin McIntosh <kmcintosh@groupon.com>
2017-10-22 14:55:51 -0700

    Syncing current state

--- src/index.js
+++ src/index.js
@@ -1,68 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/output/index.css';
-import Clock from './Clock.js'
+import Clock from './Clock.js';
+import Login from './Login.js';
 // import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-
-// function formatDate(date) {
-//   var hours = date.getHours();
-//   var minutes = date.getMinutes();
-//   var ampm = hours >= 12 ? 'pm' : 'am';
-//   hours = hours % 12;
-//   hours = hours ? hours : 12; // the hour '0' should be '12'
-//   minutes = minutes < 10 ? '0'+minutes : minutes;
-//   var strTime = hours + ':' + minutes + ' ' + ampm;
-//   return date.getMonth()+1 + "/" + date.getDate() + "/" + date.getFullYear() + "  " + strTime;
-// }
-
-// function Welcome(props) {
-//   return <h1>Hello, {props.name}</h1>;
-// }
-
-// function UserInfo(props) {
-//   return (
-//     <div className="UserInfo">
-//       <Avatar user={props.user} />
-//       <div className="UserInfo-name">
-//         {props.user.name}
-//       </div>
-//     </div>
-//   );
-// }
-
-// function Avatar(props) {
-//   return (
-//     <img className="Avatar"
-//       src={props.user.avatarUrl}
-//       alt={props.user.name}
-//     />
-//   );
-// }
-
-// function Comment(props) {
-//   return (
-//     <div className="Comment">
-//       <UserInfo user={props.author} />
-//       <div className="Comment-text">
-//         {props.text}
-//       </div>
-//       <div className="Comment-date">
-//         {formatDate(props.date)}
-//       </div>
-//     </div>
-//   );
-// }
-
-// function App() {
-//   return (
-//     <div>
-//       <Welcome name="Sara" />
-//       <Welcome name="Cahal" />
-//       <Welcome name="Edite" />
-//     </div>
-//   );
-// }
 
 class Toggle extends React.Component {
   constructor(props) {
@@ -88,32 +30,20 @@ class Toggle extends React.Component {
   }
 }
 
+const numbers = [1, 2, 3, 4, 5];
+function NumberList(props) {
+  const numbers = props.numbers;
+  const listItems = numbers.map((number) =>
+    <li key={number.toString()}>{number}</li>
+  );
+  return (
+    <ul>{listItems}</ul>
+  );
+}
+
 ReactDOM.render(
-  <Toggle />,
+  <NumberList numbers={numbers} />,
   document.getElementById('root')
 );
-
-// const comment = {
-//   date: new Date(),
-//   text: 'I hope you enjoy learning React!',
-//   author: {
-//     name: 'Hello Kitty',
-//     avatarUrl: 'http://placekitten.com/g/64/64'
-//   }
-// };
-
-// ReactDOM.render(
-//   <div>
-//     <Clock foo = ""/>
-//     <Clock />
-//     <Clock />
-//   </div>,
-//   document.getElementById('root')
-// );
-
-// ReactDOM.render(
-//   <Comment author={comment.author} text={comment.text} date={comment.date}  />,
-//   document.getElementById('root')
-// );
 
 registerServiceWorker();
