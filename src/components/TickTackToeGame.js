6b4743818429875cf85299c29e9c9bd9311f66b3
Kevin McIntosh <kmcintosh@groupon.com>
2017-10-31 20:41:46 -0700

    got grid sizing, but with some bugs

--- src/components/TickTackToeGame.js
+++ src/components/TickTackToeGame.js
@@ -16,12 +16,18 @@ export default class TickTackToeGame extends React.Component {
     this.state = {
       size: 3,
       width: 30,
-      height: 18
+      height: 18,
+      grid: []
     }
 
     this.updateHeight = this.updateHeight.bind(this);
     this.updateWidth = this.updateWidth.bind(this);
     this.updateSize = this.updateSize.bind(this);
+    this.initValues = this.initValues.bind(this);
+  }
+
+  componentDidMount() {
+    this.initValues(this.state.size);
   }
   
   updateHeight(event){
@@ -33,25 +39,41 @@ export default class TickTackToeGame extends React.Component {
   }
 
   updateSize(event){
+    let size = tryConvert(event.target.value);
     this.setState({
-        size: tryConvert(event.target.value)
+        size: size
+    });
+    this.initValues(size);
+  }
+
+  initValues(size){
+    let values = [];
+    for(let i = 0; i < size; i++){
+      values[i] = [];
+      for(let j = 0; j < size; j++){
+        values[i].push("X");
+      }
+    }
+
+    this.setState({
+      grid: values
     })
   }
 
   render(){
-    const dummyValues = this.state
-    
-    return <div>
-              <div>
-                <TickTackRow values={["X", "O", "X"]}/>
-                <TickTackRow values={["X", "O", "X"]}/>
-                <TickTackRow values={["X", "O", "X"]}/>
-              </div>
-              <div>
-                <div>Size: <input value={this.state.size} onChange={this.updateSize} /></div>
-                <div>Width: <input onChange={this.updateWidth} /></div>
-                <div>Height: <input onChange={this.updateHeight} /></div>
-              </div>
-          </div>
+    return (
+      <div>
+        <div>
+          <div>Size: <input value={this.state.size} onChange={this.updateSize} /></div>
+          <div>Width: <input onChange={this.updateWidth} /></div>
+          <div>Height: <input onChange={this.updateHeight} /></div>
+        </div>
+        <div>
+          {this.state.grid.map((row, index) => {
+            return <TickTackRow values={row} key={index}/>
+          })}
+        </div>
+      </div>
+    )
   }
 }
