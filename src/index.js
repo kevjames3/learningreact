6b54d5eb56768f0c53887e2340037977af8eacba
Kevin McIntosh <kmcintosh@groupon.com>
2017-11-01 08:37:11 -0700

    Removed un-needed statements

--- src/index.js
+++ src/index.js
@@ -1,10 +1,8 @@
 import TickTackToeGame from './components/TickTackToeGame';
-import TickTackToeCell from './components/TickTackToeCell';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './styles/output/index.css';
 import registerServiceWorker from './registerServiceWorker';
-import TickTackRow from './components/TickTackRow';
 
 const mountNode = document.getElementById('root');
 
